faa74d7bf701022bc0e770be32dd063d06232c5b
wata toshiki <wata.hal.student@gmail.com>
2025-10-02 12:23:24 +0900

    feat: Implement basic page routing

---
+++ src/pages/Blog.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Blog: React.FC = () => {
+  return (
+    <div>
+      <h1>Blog</h1>
+      <p>Welcome to my tech blog.</p>
+    </div>
+  )
+}
+
+export default Blog
---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      <h1>Home</h1>
+      <p>Welcome to my portfolio and blog!</p>
+    </div>
+  )
+}
+
+export default Home
