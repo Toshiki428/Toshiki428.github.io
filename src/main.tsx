accbd71f19790e8f63bcdd1b8a15ac6fa7701ba3
wata toshiki <wata.hal.student@gmail.com>
2025-10-05 11:12:25 +0900

    build: Configure GitHub Pages deployment

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,14 @@ window.Buffer = Buffer
 
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 )
