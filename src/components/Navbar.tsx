cc1aea38db9228abcaf2da66edcf1cfc5661c7df
wata toshiki <wata.hal.student@gmail.com>
2025-10-08 12:31:56 +0900

    style: Update favicon and header navigation

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,41 +1,38 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import logo from '/logo.svg';
 
 const Navbar: React.FC = () => {
   return (
     <nav className="bg-gray-800 p-4">
-      <ul className="flex justify-center space-x-6">
-        <li>
-          <NavLink
-            to="/"
-            className={({ isActive }) =>
-              isActive ? 'text-white font-bold underline' : 'text-gray-300 hover:text-white'
-            }
-          >
-            Home
-          </NavLink>
-        </li>
-        <li>
-          <NavLink
-            to="/portfolio"
-            className={({ isActive }) =>
-              isActive ? 'text-white font-bold underline' : 'text-gray-300 hover:text-white'
-            }
-          >
-            Portfolio
-          </NavLink>
-        </li>
-        <li>
-          <NavLink
-            to="/blog"
-            className={({ isActive }) =>
-              isActive ? 'text-white font-bold underline' : 'text-gray-300 hover:text-white'
-            }
-          >
-            Blog
-          </NavLink>
-        </li>
-      </ul>
+      <div className="container mx-auto flex items-center justify-between">
+        <NavLink to="/" className="flex items-center space-x-3">
+          <img src={logo} alt="logo" className="h-8 w-8" />
+          <span className="text-white font-bold text-xl">Toshiki's Portfolio & Blog</span>
+        </NavLink>
+        <ul className="flex space-x-6">
+          <li>
+            <NavLink
+              to="/portfolio"
+              className={({ isActive }) =>
+                isActive ? 'text-white font-bold underline' : 'text-gray-300 hover:text-white'
+              }
+            >
+              Portfolio
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/blog"
+              className={({ isActive }) =>
+                isActive ? 'text-white font-bold underline' : 'text-gray-300 hover:text-white'
+              }
+            >
+              Blog
+            </NavLink>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 };
